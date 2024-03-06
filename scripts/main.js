d672eece623d49e0cbfa82e97350d3fd2213561d
Daniel Leite <leitepereira.daniel2005@gmail.com>
2024-03-06 19:52:10 -0300

    feat: add hover in nav and hamburguer menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     window.addEventListener('scroll', function(e) {
-        if (window.scrollY > 50) {
+        if (window.scrollY > 0) {
             document.querySelector("header").classList.add('is-scrolling');
         } else {
             document.querySelector("header").classList.remove('is-scrolling')
